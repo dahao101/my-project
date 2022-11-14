bff48aec5f14d16ffa39798ed736f24961ab868b
mrt_ervn <115510395+dahao101@users.noreply.github.com>
2022-11-14 15:06:11 +0800

    Update vendo machine.c

--- vendo machine.c
+++ vendo machine.c
@@ -3,14 +3,18 @@ int main() {
      int  choice,money, coke = 35, sprite = 55, royal = 50, dew = 40;
      float change;
      char repeat = 'y';
+     
     printf("____________________vendo machine__________________________\n\n");
     while (repeat == 'y')
     {
-    printf("[1] coke [2] sprite [3] royal [4] mountain dew\n\n");
+    printf("[1] coke P35 [2] sprite P55 \n[3] royal P50 [4] mountain dew P40\n\n");
     printf("input choice: ");
     scanf("%d", & choice);
     printf("___________________________________________________________\n\n");
-    
+    if (choice != 1 && choice != 2 && choice != 3 && choice != 4)
+    {
+        printf("[invalid choice]");
+    }
         switch(choice)
         {
                 case 1:
@@ -20,6 +24,7 @@ int main() {
                 if (money >= coke)
                 {
                 change = money - coke;
+                printf("\n\nYour change is P%.2f", change);
                 }
                 else if (money < coke)
                 {
@@ -29,11 +34,12 @@ int main() {
          
         case 2:
                 printf("Thankyou for purchasing sprite! \n\n");
-                printf("enter money: ");
+                printf("enter money: P");
                 scanf("%d", & money);
                 if (money >= sprite)
                 {
                 change = money - sprite;
+                printf("\n\nYour change is P%.2f", change);
                 }
                 else if (money < sprite)
                 {
@@ -42,11 +48,12 @@ int main() {
                 break;
             case 3:
                 printf("Thankyou for purchasing royal! \n\n");
-                printf("enter money: ");
+                printf("enter money: P");
                 scanf("%d", & money);
                 if (money >= royal)
                 {
                 change = money - royal;
+                printf("\n\nYour change is P%.2f", change);
                 }
                 else if (money < royal)
                 {
@@ -56,11 +63,12 @@ int main() {
             
             case 4:
                 printf("Thankyou for purchasing mountain dew! \n\n");
-                printf("enter money: ");
+                printf("enter money: P");
                 scanf("%d", & money);
                 if (money >= dew)
                 {
                 change = money - dew;
+                printf("\n\nYour change is P%.2f", change);
                 }
                 else if (money < dew)
                 {
@@ -68,11 +76,11 @@ int main() {
                 }
                 break;
     }
-                printf("\n\nYour change is P%.2f", change);
                 printf("\n\nprogram by: mrt ervn\n\n");
                 printf("do you want to repeat? y/n\n");
                 printf("choice: ");
-                scanf(" %c",& repeat);
+                scanf(" %c", &repeat);
     }
-    return 0;
+printf("thankyou try again!");
+ return 0;
 }
